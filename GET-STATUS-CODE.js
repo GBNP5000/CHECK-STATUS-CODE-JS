12ee5320f45cf2dfd018cda55920fc43cb598f99
GBNP5000 <41781833+GBNP5000@users.noreply.github.com>
2022-08-16 20:30:38 +0530

    Create GET-STATUS-CODE.js

---
+++ GET-STATUS-CODE.js
@@ -0,0 +1,52 @@
+/*
+      SOURCE CODE 
+               |
+               v
+      https://www.youtube.com/watch?v=Gjbr21JLfgg&t=609s
+      
+      https://www.youtube.com/watch?v=R3tZ3FtTluQ
+*/
+
+lornt=false;
+
+ function xhrRequest(){            
+  
+  if (this.status == 200)
+    {
+      return (lornt=true);
+    }
+  else
+    {
+      return (lornt=false);
+    }
+}
+
+async function getReq(url){
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", xhrRequest);
+  oReq.open("GET", url);
+  oReq.send();
+}
+
+const chkstatus =async () => {
+    return new Promise((resolve,reject) => {
+      setTimeout(() => {
+        getReq(window.location.href);
+        console.log("chkstatus - LORNT :  "+String(lornt))
+        resolve(lornt);
+      },3000);
+    });
+};
+
+chk=false
+
+console.clear()
+i=0;
+while((i<10) || (chk === false))
+  {
+    chk = await chkstatus();
+    console.log("LORNT :  "+lornt)
+    console.log("I :  "+ String(i))
+     i=(i+1);
+  }
+console.log(chk)
